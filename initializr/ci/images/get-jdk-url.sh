35f5c706c475a08900ce25f9d78641cfeb1de0ae
ChenQi <chenqi92104@icloud.com>
2023-03-20 09:13:18 +0800

    init

---
+++ initializr/ci/images/get-jdk-url.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+
+case "$1" in
+	java17)
+		 echo "https://github.com/bell-sw/Liberica/releases/download/17.0.4.1+1/bellsoft-jdk17.0.4.1+1-linux-amd64.tar.gz"
+	;;
+  *)
+		echo $"Unknown java version"
+		exit 1
+esac
